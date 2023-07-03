0dc21413fb8ab7edb97037fe22caee92f00afe95
Nicola Picciafuoco <s1099360@studenti.univpm.it>
2023-07-03 16:00:26 +0200

    implementazione dati nome e cognome  presi da firestore

--- lib/widgets/login/profilo.dart
+++ lib/widgets/login/profilo.dart
@@ -1,51 +1,68 @@
-import 'package:filmaccio_flutter/widgets/login/RegTerza.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../Services/UserService.dart';
 import '../models/UserModel.dart';
 import 'modificaUtente.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../login/Auth.dart';
-class Profilo extends StatelessWidget {
-  const Profilo({Key? key}) : super(key: key);
 
-  get modificaUtente => null;
+class Profilo extends StatefulWidget {
+  Profilo({Key? key}) : super(key: key);
+
+  @override
+  _ProfiloState createState() => _ProfiloState();
+}
+
+class _ProfiloState extends State<Profilo> {
+  Future<DocumentSnapshot>? userDocFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    final User? currentUser = Auth().currentUser;
+    if (currentUser != null) {
+      userDocFuture = FirebaseFirestore.instance.collection('users').doc(currentUser.uid).get();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<UserModel>(
-      stream: UserService().user,
-      builder: (BuildContext context, AsyncSnapshot<UserModel> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        } else {
-          final UserModel? currentUser = snapshot.data;
-          final String? nameShown = currentUser?.nameShown;
-          final String? email = currentUser?.email;
-
-          return Scaffold(
-            body: SafeArea(
-              child: Column(
-                children: [
-                  Expanded(
-                    child: ListView(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                nameShown ?? 'Unknown User',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
+    return FutureBuilder<DocumentSnapshot>(
+        future: userDocFuture,
+        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot)
+    {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return Center(child: CircularProgressIndicator());
+      } else if (snapshot.hasError) {
+        return Text('Errore: ${snapshot.error}');
+      } else {
+        final DocumentSnapshot? userDoc = snapshot.data;
+        final String? nameShown = userDoc?.get('nameShown');
+        final String? email = userDoc?.get('email');
+        final String? username = userDoc?.get('username');
+        return Scaffold(
+          body: SafeArea(
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              nameShown ?? 'Unknown User',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
                               ),
-                              Text(
-                                email ?? 'No Email',
-                                style: TextStyle(fontSize: 14),
-                              ),
+                            ),
+                            Text(
+                              username ?? 'username sconosciuto',
+                              style: TextStyle(fontSize: 14),
+                            ),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
@@ -79,128 +96,127 @@ class Profilo extends StatelessWidget {
                             ),
 
                             SizedBox(height: 8),
-                              SingleChildScrollView(
-                                scrollDirection: Axis.horizontal,
-                                child: Row(
-                                  children: [
-                                    Card(
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          Text('TEMPO FILM'),
-                                          Row(
-                                            children: [
-                                              Column(
-                                                children: [
-                                                  Text(
-                                                    '2',
-                                                    style: TextStyle(
-                                                      fontWeight: FontWeight.bold,
-                                                    ),
+                            SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: Row(
+                                children: [
+                                  Card(
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment
+                                          .center,
+                                      children: [
+                                        Text('TEMPO FILM'),
+                                        Row(
+                                          children: [
+                                            Column(
+                                              children: [
+                                                Text(
+                                                  '2',
+                                                  style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
                                                   ),
-                                                  Text('mesi'),
-                                                ],
-                                              ),
-                                              SizedBox(width: 8),
-                                              Column(
-                                                children: [
-                                                  Text(
-                                                    '15',
-                                                    style: TextStyle(
-                                                      fontWeight: FontWeight.bold,
-                                                    ),
+                                                ),
+                                                Text('mesi'),
+                                              ],
+                                            ),
+                                            SizedBox(width: 8),
+                                            Column(
+                                              children: [
+                                                Text(
+                                                  '15',
+                                                  style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
                                                   ),
-                                                  Text('giorni'),
-                                                ],
-                                              ),
-                                              SizedBox(width: 8),
-                                              Column(
-                                                children: [
-                                                  Text(
-                                                    '10',
-                                                    style: TextStyle(
-                                                      fontWeight: FontWeight.bold,
-                                                    ),
+                                                ),
+                                                Text('giorni'),
+                                              ],
+                                            ),
+                                            SizedBox(width: 8),
+                                            Column(
+                                              children: [
+                                                Text(
+                                                  '10',
+                                                  style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
                                                   ),
-                                                  Text('ore'),
-                                                ],
-                                              ),
-                                            ],
-                                          ),
-                                        ],
-                                      ),
+                                                ),
+                                                Text('ore'),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                      ],
                                     ),
-                                    // Add other cards here...
-                                  ],
-                                ),
-                              ),
-                              Divider(),
-                              Text('Liste'),
-                              SizedBox(height: 8),
-                              SingleChildScrollView(
-                                scrollDirection: Axis.horizontal,
-                                child: Row(
-                                  children: [
-                                    Card(
-                                      child: Container(
-                                        width: 150,
-                                        height: 120,
-                                        child: Container(),
-                                      ),
-                                    ),
-                                    // Add other cards here...
-                                  ],
-                                ),
-                              ),
-                              Divider(),
-                              Text('Serie TV viste'),
-                              ElevatedButton(
-                                onPressed: () {},
-                                child: Text('Visualizza tutte'),
-                                style: ElevatedButton.styleFrom(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(8),
                                   ),
-                                ),
+                                  // Add other cards here...
+                                ],
                               ),
-                              SingleChildScrollView(
-                                scrollDirection: Axis.horizontal,
-                                child: Row(
-                                  children: [
-                                    Card(
-                                      child: Container(
-                                        width: 100,
-                                        height: 150,
-                                        child: Container(),
-                                      ),
+                            ),
+                            Divider(),
+                            Text('Liste'),
+                            SizedBox(height: 8),
+                            SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: Row(
+                                children: [
+                                  Card(
+                                    child: Container(
+                                      width: 150,
+                                      height: 120,
+                                      child: Container(),
                                     ),
-                                    // Add other cards here...
-                                  ],
+                                  ),
+                                  // Add other cards here...
+                                ],
+                              ),
+                            ),
+                            Divider(),
+                            Text('Serie TV viste'),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Text('Visualizza tutte'),
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
                                 ),
                               ),
-                            ],
-                          ),
+                            ),
+                            SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: Row(
+                                children: [
+                                  Card(
+                                    child: Container(
+                                      width: 100,
+                                      height: 150,
+                                      child: Container(),
+                                    ),
+                                  ),
+                                  // Add other cards here...
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          );
-        }
-      },
+          ),
+        );
+      }
+    },
     );
   }
-  void logoutFunction() async {
-    try {
-      await FirebaseAuth.instance.signOut();
-      // Aggiungi qui eventuali altre operazioni di pulizia o navigazione dopo il logout
-    } catch (e) {
-      // Gestisci eventuali errori durante il logout
-      print('Errore durante il logout: $e');
+        void logoutFunction() async {
+      try {
+        await Auth().signOut();
+        // Aggiungi qui eventuali altre operazioni di pulizia o navigazione dopo il logout
+      } catch (e) {
+        // Gestisci eventuali errori durante il logout
+        print('Errore durante il logout: $e');
+      }
+      }
     }
-  }
-  
-
-}
