23fffcfd38f13fb10d08b61ebd678f30ba171cb3
Nicola Picciafuoco <s1099360@studenti.univpm.it>
2023-07-09 09:57:44 +0200

    sistemazione classi

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-import 'package:filmaccio_flutter/widgets/login/RegTerza.dart';
 import 'package:filmaccio_flutter/widgets/nav/bottomnavbar.dart';
-
-import 'package:filmaccio_flutter/widgets/login/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
-
 import 'package:flutter/material.dart';
 import 'widgets/login/login.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -12,29 +8,25 @@ import 'firebase_options.dart';
 
 import 'color_schemes.g.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-
-   MyApp({super.key});
-
+  MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
     return MaterialApp(
-
         title: 'Filmaccio',
         debugShowCheckedModeBanner: false,
         home: StreamBuilder(
@@ -47,14 +39,7 @@ class _MyAppState extends State<MyApp> {
             }
           },
         ),
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: lightColorScheme
-      ),
-      darkTheme: ThemeData(
-        useMaterial3: true,
-        colorScheme: darkColorScheme
-      )
-    );
+        theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+        darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme));
   }
 }
